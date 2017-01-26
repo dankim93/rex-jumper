957d13e467f27f009c35db2c66c26017bf2b8a98
dankim93 <dan_kim93@hotmail.com>
2017-01-25 16:20:55 -0800

    Fix cloud and restart bugs and add background

--- bundle.js
+++ bundle.js
@@ -52,6 +52,8 @@
 	  canvas.height = Game.DIM_Y;
 	  var ctx = canvas.getContext("2d");
 	
+	  const music = new Audio("./assets/audio/beginning.mp3");
+	  music.play();
 	
 	  const game = new Game(ctx);
 	  document.addEventListener('keydown', e => {
@@ -70,6 +72,7 @@
 	const Cactus = __webpack_require__(2);
 	const Player = __webpack_require__(3);
 	const Bird = __webpack_require__(4);
+	const Cloud = __webpack_require__(5);
 	
 	class Game {
 	  constructor(ctx) {
@@ -78,6 +81,7 @@
 	    this.player = new Player();
 	    this.cactus = new Cactus();
 	    this.bird = new Bird();
+	    this.cloud = new Cloud();
 	    this.collided = false;
 	    // this.init = setInterval(this.draw.bind(this), 3);
 	    this.start();
@@ -93,8 +97,14 @@
 	
 	  drawScore() {
 	    this.ctx.font = "16px Arial";
-	
 	    this.ctx.fillText("Score: "+this.score, 8, 20);
+	  }
+	
+	  drawGameover() {
+	    this.ctx.font = "50px Arial";
+	    this.ctx.fillText("Game Over", 180, 150);
+	    this.ctx.font = "15px Arial";
+	    this.ctx.fillText("Press 'Enter' to restart", 230, 190);
 	  }
 	
 	  draw(){
@@ -102,7 +112,8 @@
 	    this.cactus.drawCactus(this.ctx);
 	    this.player.drawRex(this.ctx);
 	    this.bird.drawBird(this.ctx);
-	    this.collision(this.player, this.cactus);
+	    this.cloud.drawCloud(this.ctx);
+	    this.collision(this.player, this.cactus, this.bird);
 	    this.drawGround();
 	
 	    this.score += 1;
@@ -123,16 +134,22 @@
 	      if (this.collided) {
 	        this.cactus.DIM_X = 600;
 	        this.cactus.DIM_Y = 280;
+	        this.cactus.dx = -1;
 	        this.player.DIM_X = 90;
 	        this.player.DIM_Y = 280;
 	        this.player.dy = -1.5;
-	        this.cactus.dx = -1;
+	        this.bird.DIM_X = 600;
+	        this.bird.DIM_Y = 240;
+	        this.bird.dx = -0.5;
 	        this.start();
+	      } else {
+	        this.player.dy = -1.5;
+	        this.cactus.dx = -1;
 	      }
 	    }
 	  }
 	
-	  collision(rect1, rect2) {
+	  collision(rect1, rect2, rect3) {
 	    if (rect1.DIM_X < rect2.DIM_X + 20 &&
 	        rect1.DIM_X + 20 > rect2.DIM_X &&
 	        rect1.DIM_Y < rect2.DIM_Y + 20 &&
@@ -140,13 +157,23 @@
 	          // clearInterval(this.start);
 	          clearInterval(this.init);
 	          this.collided = true;
+	          this.drawGameover();
+	
 	          const music = new Audio("./assets/audio/lose.mp3");
 	          music.play();
 	
+	        } else if (rect1.DIM_X < rect3.DIM_X + 20 &&
+	            rect1.DIM_X + 20 > rect3.DIM_X &&
+	            rect1.DIM_Y < rect3.DIM_Y + 20 &&
+	            20 + rect1.DIM_Y > rect3.DIM_Y) {
+	              clearInterval(this.init);
+	              this.collided = true;
+	              this.drawGameover();
 	        }
 	  }
 	
 	  start() {
+	    this.score = 0;
 	    this.init = setInterval(this.draw.bind(this), 3);
 	  }
 	}
@@ -177,10 +204,10 @@
 	    ctx.fill();
 	    ctx.closePath();
 	    this.DIM_X += this.dx;
-	    if (this.DIM_X === 0) {
-	      this.dx = -1 * this.dx;
-	    } else if (this.DIM_X === 580) {
-	      this.dx = -1;
+	
+	    if (this.DIM_X < 0) {
+	      this.DIM_X = 600;
+	      // this.dx = -1 * Math.random() * 3;
 	    }
 	  }
 	}
@@ -210,7 +237,7 @@
 	    ctx.closePath();
 	    this.DIM_Y += this.dy;
 	
-	    if (this.DIM_Y <= 210) {
+	    if (this.DIM_Y <= 100) {
 	      this.dy = -1 * this.dy;
 	      // this.dy += 0.2;
 	    } else if (this.DIM_Y >= 280) {
@@ -219,11 +246,10 @@
 	  }
 	
 	  jump(ctx) {
-	    const music = new Audio("./assets/audio/jump.mp3");
-	    music.play();
-	
 	    if (this.DIM_Y >= 280) {
-	      this.dy = -1.5;
+	      this.dy = -2;
+	      const music = new Audio("./assets/audio/jump.mp3");
+	      music.play();
 	    }
 	  }
 	}
@@ -258,6 +284,35 @@
 	}
 	
 	module.exports = Bird;
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	class Cloud{
+	  constructor() {
+	    this.DIM_X = 600;
+	    this.DIM_Y = 100;
+	    this.dx = -0.2;
+	    this.img = document.getElementById('cloud');
+	  }
+	  drawCloud(ctx) {
+	    ctx.beginPath();
+	    // ctx.arc(this.DIM_X, this.DIM_Y, 10, 0, Math.PI*2);
+	    // ctx.rect(this.DIM_X, this.DIM_Y, 20, 20);
+	    ctx.drawImage(this.img, this.DIM_X, this.DIM_Y, 250, 100);
+	    ctx.fill();
+	    ctx.closePath();
+	    this.DIM_X += this.dx;
+	    // console.log(this.DIM_X);
+	    if (this.DIM_X < -240) {
+	       this.DIM_X = 600;
+	    }
+	  }
+	}
+	
+	module.exports = Cloud;
 
 
 /***/ }
